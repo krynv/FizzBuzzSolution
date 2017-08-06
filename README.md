59cf2ee99a3b2729e9e11445037261e294bc23ad
VK <DEFCONone1@users.noreply.github.com>
2017-08-06 11:03:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My solution to the FizzBuzz interview question/ puzzle
+
+Count to 100, every time the number is a
+multiple of 3 - output: Fizz,
+multiple of 5 - output: Buzz
+and if the number is a multiple of both - output: FizzBuzz
+
---
+++ myFizzBuzzSolution.js
@@ -0,0 +1,13 @@
+// Vitaliy Krynytskyy <vitaliy.krynytskyy@gmail.com>
+// https://github.com/krynv
+
+for (var i = 1; i <= 100; i++) {
+    var outputString = '';
+
+    if (i % 3 == 0) { outputString += 'Fizz'; }
+    if (i % 5 == 0) { outputString += 'Buzz'; }
+
+    if (!outputString) { outputString = i; }
+
+    console.log(outputString);
+}
